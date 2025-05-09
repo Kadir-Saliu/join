1217f899376f75c79996f3c7b74f18346388ba54
danielkrafzik <daniel.krafzik@hotmail.de>
2025-05-09 14:37:35 +0200

    started Html + Styling Login

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 const BASE_URL = "https://join-3193b-default-rtdb.europe-west1.firebasedatabase.app/";
-function init() {}
+function init() {
+    setTimeout(addDisplayToContent, 2500);
+}
+
+/**
+ * shows start content, after the start animation finished
+ */
+function addDisplayToContent() {
+    document.getElementById("header-div").classList.remove("animation-hide");
+    document.getElementById("main").classList.remove("animation-hide");
+    document.getElementById("footer").classList.remove("animation-hide");
+};
