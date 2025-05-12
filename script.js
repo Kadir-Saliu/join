a49544fb5e2238211d24cacb2d647d457a917128
danielkrafzik <daniel.krafzik@hotmail.de>
2025-05-12 23:20:01 +0200

    first implementions of log in logic

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const BASE_URL = "https://join-3193b-default-rtdb.europe-west1.firebasedatabase.app/";
+const BASE_URL = "https://join-3193b-default-rtdb.europe-west1.firebasedatabase.app/.json";
+
 function init() {
     setTimeout(addDisplayToContent, 2500);
 }
@@ -19,4 +20,28 @@ function toogleInputMenu() {
     document.getElementById("header-div").classList.toggle("animation-hide");
     document.getElementById("login-div").classList.toggle("animation-hide");
     document.getElementById("sign-up-div").classList.toggle("animation-hide");
+}
+
+async function getFirebaseData(event) {
+    event.preventDefault();
+    try {
+    let response = await fetch(BASE_URL);
+    let responseJson = await response.json();
+    checkLoginData(responseJson);
+    console.log(responseJson);
+    
+  } catch (error) {
+    console.log("error")
+  };
+}
+
+async function checkLoginData(data) {
+    for (let i = 0; i < data.contacts.length; i++) {
+        if(!data.contacts[i]) continue;
+        if(document.getElementById("email-input").value === data.contacts[i].Email) {
+            return console.log("success");            
+        } else {
+            console.log("who are you?");            
+        }        
+    }
 }
