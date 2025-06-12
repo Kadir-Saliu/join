a5bab38ca30aa92d600c574a0c0012975f11bde8
Iraqi1994 <131317527+Iraqi1994@users.noreply.github.com>
2025-06-12 15:19:19 +0200

    added functionality to check if user or guest is logged in and display the name of the user on the summary page and the initals in the profile button if a user is logged in

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 const BASE_URL = "https://join-3193b-default-rtdb.europe-west1.firebasedatabase.app/.json";
+let loggedInUser = JSON.parse(localStorage.getItem("loggedInUser")) || {
+  username: "",
+  initals: "",
+};
 
 function init() {
   includeHTML();
   setTimeout(addDisplayToContent, 2500);
   const logo = document.querySelector(".slide-out-tl");
-  const wrapper = document.getElementById("logo-wrapper");
   logo.addEventListener("animationend", () => {
     document.getElementById("header-div").classList.remove("animation-hide");
     document.getElementById("login-div").classList.remove("animation-hide");
     document.getElementById("footer").classList.remove("animation-hide");
   });
+}
+
+function summaryInit() {
+  includeHTML();
+  greetUser();
 }
 
 /**
@@ -22,7 +30,6 @@ async function getFirebaseData(event) {
     let response = await fetch(BASE_URL);
     let responseJson = await response.json();
     checkLoginData(responseJson);
-    console.log(responseJson);
   } catch (error) {
     console.log("error");
   }
@@ -42,7 +49,27 @@ async function includeHTML() {
   }
 }
 
-
 function popUpAccNav() {
   document.getElementsByClassName("account-nav-render-div")[0].classList.toggle("hide");
+}
+
+/**
+ * This function removes the username and intitals from the local storage
+ */
+function removeUserfromLocalStorage() {
+  localStorage.removeItem("loggedInUser");
+}
+
+/**
+ * This function checks if a user is logged in and greets them. If the guest login is used, the greeting contains only "Good morning".
+ */
+function greetUser() {
+  if (loggedInUser.username) {
+    document.getElementById("goodMorning").innerText = "Good morning,";
+    document.getElementById("username").innerText = loggedInUser.username;
+    document.getElementById("profile").innerText = loggedInUser.initals;
+  } else {
+    document.getElementById("goodMorning").innerText = "Good morning";
+    document.getElementById("username").innerText = "";
+  }
 }
